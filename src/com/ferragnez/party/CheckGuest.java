8d69b4ee51ecafa044807db204573bcf7ec11d2c
DanielemarchioP <danielemarchio709@gmail.com>
2024-04-20 02:30:17 +0200

    esercizio terminato ma non funzionante...
    a mio parere l'esercizio e' scritto in maniera corretta, ma non so il perche non parte l' IF

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,32 @@
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+
+	public static void main(String[] args) {
+					
+		String [] nomi = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+		
+		System.out.println("Inserisci un nome da cercare all'interno della lista invitati ");
+			Scanner scan = new Scanner(System.in);
+			
+			String cercaNome = scan.nextLine();
+			 
+			 System.out.println(cercaNome);
+			 
+			for(int i = 0 ; i < nomi.length ; i++){
+				
+				if(cercaNome == nomi[i]) {
+					
+					System.out.print("L'invitato puo entrare alla festa dei Ferragnez siuuum");
+					
+				}else if(i == i-1){
+					System.out.print("L'invitato non e ben accetto alla festa");
+				}
+			}
+			scan.close();
+	}
+
+}
+
